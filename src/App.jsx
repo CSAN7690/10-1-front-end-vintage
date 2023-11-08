2edc9c3e3fbc2d1509938242813c60c40daaeca7
christophersanchez <christophersanchez@pursuit.org>
2023-11-08 17:07:04 -0500

    Update 🚧

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import IndexPage from './indexPage';
-import ShowPage from './showPage';
-import CreatePage from './createPage';
+import IndexPage from './IndexPage';
+import ShowPage from './ShowPage';
+import CreatePage from './CreatePage';
+import EditPage from './EditPage'
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
+
         {/* Navigation Bar */}
         <nav>
           <Link to="/">home</Link>
           <Link to="/create">createNewResource</Link>
         </nav>
+
         {/* Routes */}
         <Switch>
           <Route path="/" exact component={IndexPage} />
           <Route path="/show/:id" component={ShowPage} />
           <Route path="/create" component={CreatePage} />
+          <Route path="edit/:id" component={EditPage} />
         </Switch>
       </div>
     </Router>
