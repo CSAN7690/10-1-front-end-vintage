5159ee9ccf1b0ac7f5c275edc362ebdafb1f88ec
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-11-09 09:45:26 -0500

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import './App.css';
 import HomePage from "./Components/HomePage.jsx"
 import ShowPage from './Components/showPage';
 import NavBar from './Components/NavBar';
-
+import EditPage from './Components/EditPage';
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
       <NavBar />
-      <Routes>
+    <div className="App">
+       <Routes>
+
+      
+      
+  
       <Route path='/'element={<HomePage />} />
       <Route path='/vintage'element={<ShowPage />} />
       <Route path='/vintage/:id'element={<EditPage/>} />
   
-    </Routes>
+      </Routes>
+      </div>
    </Router>
-</div>
- 
+    
+
+  
+  
           
   );
 }
