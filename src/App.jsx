99bacf9165d44c6209137809c74a9adfc4a23800
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-11-09 07:45:34 -0500

    deleted and remodified comonents

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import IndexPage from './IndexPage';
-import ShowPage from './ShowPage';
-import CreatePage from './CreatePage';
-import EditPage from './EditPage'
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-
-        {/* Navigation Bar */}
-        <nav>
-          <Link to="/">home</Link>
-          <Link to="/create">createNewResource</Link>
-        </nav>
-
-        {/* Routes */}
-        <Switch>
-          <Route path="/" exact component={IndexPage} />
-          <Route path="/show/:id" component={ShowPage} />
-          <Route path="/create" component={CreatePage} />
-          <Route path="edit/:id" component={EditPage} />
-        </Switch>
-      </div>
-    </Router>
+   <div>
+    <h1>
+      welcome
+      
+    </h1>
+   </div>
+          
   );
 }
 
