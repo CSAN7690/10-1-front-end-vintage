e41d169f9b753a9f1d1d63bc54dbb57df47f6f65
christophersanchez <christophersanchez@pursuit.org>
2023-11-09 16:42:32 -0500

    Revert "Updated Components + data  show on browser + reacting with backend"
    This reverts commit b1df4e70c540c5740351120ce6712f7ede068a3d.

--- src/Components/indexPage.jsx
+++ src/Components/indexPage.jsx
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const IndexPage = () => {
-    const [vintageClothes, setVintageClothes] = useState([]);
-
-    useEffect(() => {
-
-        const url = "http://localhost:4005/vintage";
-
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                setVintageClothes(data);
-            })
-    }, []);
-
-
+    const resources = [];
     return (
         <div>
             <h1>Index Page</h1>
             {/* Display resources */}
             <ul>
-                {vintageClothes.map((vintageItem) => (
-                    <li key={vintageItem.id}>
-                        {vintageItem.name}
-                        {/* <Link to={`/show/${resource.id}`}>{resource.name}</Link> */}
+                {resources.map((resource) => (
+                    <li key={resource.id}>
+                        <Link to={`/show/${resource.id}`}>{resource.name}</Link>
                     </li>
                 ))}
             </ul>
