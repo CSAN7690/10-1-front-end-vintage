b1df4e70c540c5740351120ce6712f7ede068a3d
christophersanchez <christophersanchez@pursuit.org>
2023-11-09 13:08:02 -0500

    Updated Components + data  show on browser + reacting with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import IndexPage from './IndexPage';
-import ShowPage from './ShowPage';
-import CreatePage from './CreatePage';
-import EditPage from './EditPage'
+// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import IndexPage from './Components/IndexPage';
+// import ShowPage from './ShowPage';
+// import CreatePage from './CreatePage';
+// import EditPage from './EditPage'
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-
-        {/* Navigation Bar */}
-        <nav>
+    // <Router>
+    <div>
+      <IndexPage />
+      {/* Navigation Bar */}
+      {/* <nav>
           <Link to="/">home</Link>
           <Link to="/create">createNewResource</Link>
-        </nav>
+        </nav> */}
 
-        {/* Routes */}
-        <Switch>
-          <Route path="/" exact component={IndexPage} />
-          <Route path="/show/:id" component={ShowPage} />
+      {/* Routes */}
+      {/* <Switch> */}
+      {/* <Route path="/" exact component={IndexPage} /> */}
+      {/* <Route path="/show/:id" component={ShowPage} />
           <Route path="/create" component={CreatePage} />
-          <Route path="edit/:id" component={EditPage} />
-        </Switch>
-      </div>
-    </Router>
+          <Route path="edit/:id" component={EditPage} /> */}
+      {/* </Switch> */}
+    </div>
+    // </Router>
   );
 }
 
--- src/Components/indexPage.jsx
+++ src/Components/indexPage.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
 
 const IndexPage = () => {
-    const resources = [];
+    const [vintageClothes, setVintageClothes] = useState([]);
+
+    useEffect(() => {
+
+        const url = "http://localhost:4005/vintage";
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setVintageClothes(data);
+            })
+    }, []);
+
+
     return (
         <div>
             <h1>Index Page</h1>
             {/* Display resources */}
             <ul>
-                {resources.map((resource) => (
-                    <li key={resource.id}>
-                        <Link to={`/show/${resource.id}`}>{resource.name}</Link>
+                {vintageClothes.map((vintageItem) => (
+                    <li key={vintageItem.id}>
+                        {vintageItem.name}
+                        {/* <Link to={`/show/${resource.id}`}>{resource.name}</Link> */}
                     </li>
                 ))}
             </ul>
